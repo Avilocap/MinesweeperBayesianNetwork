bbb51cadf606aee31ae42e318ee86346ae3782f4
David De la torre <daviddelatorre00@gmail.com>
2018-05-08 21:07:59 +0200

    Two basic functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+import random
+
+def seleccionar_probabilidad(cpd, valor, evidencia):
+    padres = [v for v in cpd.variables if v != cpd.variable]
+    valores_evidencia = tuple(evidencia[var] for var in padres)
+    return cpd.values[valor][valores_evidencia]
+
+def generar_valor_aleatorio(cardinalidad, probabilidades):
+    p = random.random()
+    acumuladas = 0
+    for valor in range(cardinalidad):
+        acumuladas += probabilidades[valor]
+        if p <= acumuladas:
+            return valor
