0ec47549ef3a9e59e5d4ec28406e9866d7e6ec1d
David De la torre <daviddelatorre00@gmail.com>
2018-05-14 18:59:38 +0200

    more testing

--- test.py
+++ test.py
@@ -2,4 +2,6 @@ from __future__ import print_function
 import board as board
 
 borad = board.MSBoard(10,10,5)
-borad.print_board()
+#borad.print_board()
+#print(borad.info_map)
+print(borad.mine_map)
