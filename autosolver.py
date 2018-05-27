c0607d90fdab25f0f4c397b285e4115ace47e932
mruwzum <chicoantolin@gmail.com>
2018-05-27 02:06:53 +0200

    solv

--- autosolver.py
+++ autosolver.py
@@ -57,38 +57,38 @@ elif game.game_status == 1:
     print("YOU WON!")
 elif game.game_status == 2:
     no_bombas_enYij={} 
-no_bombas_enXij={} 
-evidencias= {}
-sindescubrir = []
-for i in range(board.board_width):
-    for j in range(board.board_height):
-        field_status = board.info_map[j, i]
-        if field_status in range(1,8):
-            evidencias["X" + str(i) + str(j)] = 0
-            evidencias["Y" + str(i) + str(j)] = field_status
-        elif field_status == 0:
-            evidencias["X" + str(i) + str(j)] = 0
-            evidencias["Y" + str(i) + str(j)] = 0
-        elif field_status == 11:
-            sindescubrir.append("X" + str(i) + str(j))
-print(evidencias)
+    no_bombas_enXij={} 
+    evidencias= {}
+    sindescubrir = []
+    for i in range(board.board_width):
+        for j in range(board.board_height):
+            field_status = board.info_map[j, i]
+            if field_status in range(1,8):
+                evidencias["X" + str(i) + str(j)] = 0
+                evidencias["Y" + str(i) + str(j)] = field_status
+            elif field_status == 0:
+                evidencias["X" + str(i) + str(j)] = 0
+                evidencias["Y" + str(i) + str(j)] = 0
+            elif field_status == 11:
+                sindescubrir.append("X" + str(i) + str(j))
+    print(evidencias)
 
-Model_Game_ev = pgmi.VariableElimination(modelo)
-consulta = Model_Game_ev.query(sindescubrir, evidencias)
+    Model_Game_ev = pgmi.VariableElimination(modelo)
+    consulta = Model_Game_ev.query(sindescubrir, evidencias)
 
-listaDeProbsFinales = []
-for x in range(len(sindescubrir)):
-    listaDeProbsFinales.append(consulta[sindescubrir[x]].values)
-listasCeros = [item[0] for item in listaDeProbsFinales]
-maximo = max(listasCeros)
-winner = sindescubrir[listasCeros.index(max(listasCeros))]
-i = winner[1:2]
-j = winner[2:3]
-print("Click on: "+str(i)+","+str(j))
-game.play_move("click",i.values,j.values)
-game.print_board()
-board = game.board
-print(board.info_map)
+    listaDeProbsFinales = []
+    for x in range(len(sindescubrir)):
+        listaDeProbsFinales.append(consulta[sindescubrir[x]].values)
+    listasCeros = [item[0] for item in listaDeProbsFinales]
+    maximo = max(listasCeros)
+    winner = sindescubrir[listasCeros.index(max(listasCeros))]
+    i = winner[1:2]
+    j = winner[2:3]
+    print("Click on: "+str(i)+","+str(j))
+    game.play_move("click",int(i),int(j))
+    game.print_board()
+    board = game.board
+    print(board.info_map)
 
 # while game.game_status == 2:
 #    
